d9fac7ca2b2f6c792bf096555176b263b2d21838
lvlarco <30390672+lvlarco@users.noreply.github.com>
2018-03-30 00:59:29 -0400

    Update switch.py

--- switch.py
+++ switch.py
@@ -18,8 +18,8 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(button_pin)
-    left_motor = __motor_(left_in1_pin, left_in2_pin)
-    right_motor = __motor_(right_in1_pin, right_in2_pin)
+    left_motor = self.__motor_(left_in1_pin, left_in2_pin)
+    right_motor = self.__motor_(right_in1_pin, right_in2_pin)
     
     if input_state == False:
         left_motor.clockwise()
